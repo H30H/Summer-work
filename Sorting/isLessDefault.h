ab83dbe4300c6d04406d2b9710daf5675e47d90e
Grisha <gkurennyy@mail.ru>
2021-07-14 13:07:05 +0500

    Created some sorting algorithms

---
+++ Sorting/isLessDefault.h
@@ -0,0 +1,13 @@
+//
+// Created by Grisha on 14.07.2021.
+//
+
+#ifndef BASE_CLASSES_ISLESSDEFAULT_H
+#define BASE_CLASSES_ISLESSDEFAULT_H
+
+template<typename T>
+bool isLessDefault(const T& obj1, const T& obj2) {
+    return obj1 < obj2;
+}
+
+#endif //BASE_CLASSES_ISLESSDEFAULT_H
